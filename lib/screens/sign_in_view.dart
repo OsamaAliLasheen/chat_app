d5bb231ece4eedd2b94d207633b577fd0c032e99
Osama <osama.ali.lasheen@gmail.com>
2024-06-18 15:15:00 +0300

    Refactor sign in view UI
    This commit refactors the UI of the sign in view by adding labels and borders to the email and password text fields, and adding a sign in button. It also adds a sign up button for users who don't have an account yet. The UI changes improve the user experience and make the view more visually appealing.

--- lib/screens/sign_in_view.dart
+++ lib/screens/sign_in_view.dart
@@ -5,21 +5,76 @@ class SignInView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xff274460),
+    return Scaffold(
+      backgroundColor: const Color(0xff274460),
       body: Center(
         child: Column(
           children: [
-            SizedBox(height: 100),
-            Image(image: AssetImage('assets/images/scholar.png')),
-            Text(
+            const SizedBox(height: 100),
+            const Image(image: AssetImage('assets/images/scholar.png')),
+            const Text(
               'Scholar Chat',
               style: TextStyle(
                   fontSize: 36,
                   fontWeight: FontWeight.bold,
                   fontFamily: 'pacifico',
                   color: Colors.white),
-            )
+            ),
+            const SizedBox(height: 150),
+            const Text('Sign In',
+                style: TextStyle(color: Colors.white, fontSize: 28)),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(color: Colors.white),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    )),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: TextStyle(color: Colors.white),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    )),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              height: 50,
+              width: MediaQuery.of(context).size.width,
+              child: ElevatedButton(
+                child: const Text(
+                  'Sign In',
+                  style: TextStyle(fontSize: 24, color: Color(0xff274460)),
+                ),
+                onPressed: () {},
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("don't have an account?",
+                    style: TextStyle(fontSize: 18, color: Colors.white)),
+                TextButton(
+                    onPressed: () {},
+                    child: const Text('Sign Up',
+                        style: TextStyle(fontSize: 18, color: Colors.blue)))
+              ],
+            ),
           ],
         ),
       ),
